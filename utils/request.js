7cea90af0ece89400213d80751c6da8c760b8230
Ocean Stack <lovelypurpleclothes@outlook.com>
2020-07-13 20:33:55 +0800

    add a checkout on request.js
    if didn't login account, jump to login page

--- utils/request.js
+++ utils/request.js
@@ -7,10 +7,17 @@ const request = (options) => {
   // 也就是说这是 单例模式的，无论这个文件被多少个文件引用，函数外的代码，都始终只被执行一次。
   const token = wx.getStorageSync('token');
 
-  if (token.token) {
+  if (token && token.token) {
     // 附加令牌
     if (!options.header) options.header = {};
     options.header.Authorization = `Bearer ${token.token}`;
+  } else {
+    // 如果没有token就跳去登录，这个封装的请求只用于需要验证的
+    wx.navigateTo({
+      url: "/pages/login/login"
+    })
+    // 返回函数
+    return;
   }
   // 附加请求基址
   options.url = app.config.apiUrl.concat(options.url);
