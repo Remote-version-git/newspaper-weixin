d44876574a4a4cb706924a738f17fc9b74496a05
zhangjian <zhangjian@qq.com>
2020-07-20 23:52:12 +0800

    finall version

--- components/player/player.js
+++ components/player/player.js
@@ -14,21 +14,20 @@ Component({
    * 页面的初始数据
    */
   data: {
-    // videoimage: "block" ,//默认显示封面
-    bindplay: null,
-    _index:0,
-    videos: [
-      {
-        v_id:1,
+    default: 0,
+    videos: [{
+        v_id: 1,
+        myVideo: 'myVideo1',
         cover: 'http://image2.pearvideo.com/cont/20181117/cont-1477753-11703578.png',
         avatar: 'http://image2.pearvideo.com/cont/20181117/cont-1477753-11703578.png',
         src: 'http://video.pearvideo.com/mp4/adshort/20181117/cont-1477753-13248875_adpkg-ad_hd.mp4',
         title: 'CNN起诉川普政府案宣判，CNN获胜CNN起诉川普政府案宣判，CNN获胜',
         writer: '看看新闻'
-        
+
       },
       {
-        v_id:2,
+        v_id: 2,
+        myVideo: 'myVideo2',
         cover: 'http://image.pearvideo.com/cont/20181116/cont-1477146-11703021.jpeg',
         avatar: 'http://image.pearvideo.com/node/19-10027896-logo.jpg',
         src: 'http://video.pearvideo.com/mp4/adshort/20181116/cont-1477146-13247738_adpkg-ad_hd.mp4',
@@ -36,7 +35,8 @@ Component({
         writer: '眼镜儿'
       },
       {
-        v_id:3,
+        v_id: 3,
+        myVideo: 'myVideo3',
         cover: 'http://image1.pearvideo.com/cont/20181118/cont-1478240-11705317.png',
         avatar: 'http://image.pearvideo.com/node/1548-10523493-logo.png',
         src: 'http://video.pearvideo.com/mp4/short/20181118/cont-1478240-13253290-hd.mp4',
@@ -45,23 +45,34 @@ Component({
       }
     ]
   },
-  //点击播放按钮，封面图片隐藏,播放视频
-  bindplay: function (e) {
-    var index = e.currentTarget.dataset.id;
-    var that = this;
-    this.setData({
-      index: index
-    })
-    //停止正在播放的视频
-    var videoContextPrev = wx.createVideoContext("myVideo" + that.data._index)
-    videoContextPrev.seek(0)
-    videoContextPrev.pause()
+  methods: {
+    //点击播放按钮，封面图片隐藏,播放视频
+    videoPlay: function (e) {
+      var index = e.currentTarget.dataset.id + '';
+      var that = this;
+      this.setData({
+        default: index
+      })
+      //停止正在播放的视频
+      var videoContextPrev = wx.createVideoContext("myVideo" + that.data.default)
+      videoContextPrev.seek(0)
+      videoContextPrev.pause()
+      setTimeout(function () {
+        //将点击视频进行播放
+        var videoContext = wx.createVideoContext("myVideo" + index)
+        videoContext.play();
+        that.data.default = index;
+      }, 100)
 
-    setTimeout(function () {
-      //将点击视频进行播放
-      var videoContext = wx.createVideoContext('myVideo' + index)
-      videoContext.play();
-      that.data._index = index;
-    }, 100)
+      
+    },
+  },
+  // 退出组件视频停止
+  hide: function() {
+    //离开当前页面
+    VideoContext.stop()
+  },
+  detached() {
+    console.log(1111)
   }
 })
--- miniprogram_npm/json-bigint/index.js
+++ miniprogram_npm/json-bigint/index.js
@@ -4,7 +4,7 @@ var __DEFINE__ = function(modId, func, req) { var m = { exports: {}, _tempexport
 var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = __MODS__[modId].m; m._exports = m._tempexports; var desp = Object.getOwnPropertyDescriptor(m, "exports"); if (desp && desp.configurable) Object.defineProperty(m, "exports", { set: function (val) { if(typeof val === "object" && val !== m._exports) { m._exports.__proto__ = val.__proto__; Object.keys(val).forEach(function (k) { m._exports[k] = val[k]; }); } m._tempexports = val }, get: function () { return m._tempexports; } }); __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); } return __MODS__[modId].m.exports; };
 var __REQUIRE_WILDCARD__ = function(obj) { if(obj && obj.__esModule) { return obj; } else { var newObj = {}; if(obj != null) { for(var k in obj) { if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k]; } } newObj.default = obj; return newObj; } };
 var __REQUIRE_DEFAULT__ = function(obj) { return obj && obj.__esModule ? obj.default : obj; };
-__DEFINE__(1594889702389, function(require, module, exports) {
+__DEFINE__(1595245911452, function(require, module, exports) {
 var json_stringify = require('./lib/stringify.js').stringify;
 var json_parse     = require('./lib/parse.js');
 
@@ -18,8 +18,8 @@ module.exports = function(options) {
 module.exports.parse = json_parse();
 module.exports.stringify = json_stringify;
 
-}, function(modId) {var map = {"./lib/stringify.js":1594889702390,"./lib/parse.js":1594889702391}; return __REQUIRE__(map[modId], modId); })
-__DEFINE__(1594889702390, function(require, module, exports) {
+}, function(modId) {var map = {"./lib/stringify.js":1595245911453,"./lib/parse.js":1595245911454}; return __REQUIRE__(map[modId], modId); })
+__DEFINE__(1595245911453, function(require, module, exports) {
 var BigNumber = require('bignumber.js');
 
 /*
@@ -405,7 +405,7 @@ var JSON = module.exports;
 }());
 
 }, function(modId) { var map = {}; return __REQUIRE__(map[modId], modId); })
-__DEFINE__(1594889702391, function(require, module, exports) {
+__DEFINE__(1595245911454, function(require, module, exports) {
 var BigNumber = null;
 /*
     json_parse.js
@@ -792,6 +792,6 @@ var json_parse = function (options) {
 module.exports = json_parse;
 
 }, function(modId) { var map = {}; return __REQUIRE__(map[modId], modId); })
-return __REQUIRE__(1594889702389);
+return __REQUIRE__(1595245911452);
 })()
 //# sourceMappingURL=index.js.map
